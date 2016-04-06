bd99b3ca599b4409345659187c39e3b18c2f91d1
ustask <kathleenustas@Kathleens-MacBook-Pro.local>
2016-04-05 21:39:40 -0400

    Grid hw

--- sketch.js
+++ sketch.js
@@ -16,11 +16,19 @@
     // stuff happens
   }
 */
-
 function setup() {
-
+  createCanvas(800,800);
 }
 
 function draw() {
-
+  background(115,10,40);
+  
+  for(var x=0; x <= width; x += 80) {
+      for(var y=0; y<= height; y +=80){ 
+    stroke(random(230));
+    fill(0,50,200);
+    ellipse(x,y,20,20);
+      
+  }
+}
 }
